e5fa81e9a95db0ef8bc916c34c70d58841067e4d
Pavel Semenov <semenov.pavel@lynkage.ru>
2023-06-09 16:20:13 +0300

    HW2 is completed fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,42 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"golang.org/x/example/stringutil"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var err error
+	var b strings.Builder
+	if len(s) == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(rune(s[0])) {
+		return "", ErrInvalidString
+	}
+	s = stringutil.Reverse(s)
+	tmp := rune(s[0])
+	counter := 1
+	for _, r := range s {
+		if unicode.IsDigit(r) {
+			if unicode.IsDigit(tmp) {
+				return "", ErrInvalidString
+			}
+			counter, err = strconv.Atoi(string(r))
+			if err != nil {
+				return "", err
+			}
+		} else {
+			b.WriteString(strings.Repeat(string(r), counter))
+			counter = 1
+		}
+		tmp = r
+	}
+
+	return stringutil.Reverse(b.String()), nil
 }
