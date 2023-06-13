d75e3a6b0871cc8d44bc0f4d715ee32dd25644e2
Pavel Semenov <semenov.pavel@lynkage.ru>
2023-06-13 18:24:45 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,57 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type sortItem struct {
+	key   string
+	value int
+}
+
+var re = regexp.MustCompile(`[.,()!?^&*'";:/\\|-]+`)
+
+func Top10(text string) []string {
+	data := strings.Fields(text)
+
+	if len(data) == 0 {
+		return []string{}
+	}
+
+	wordFrequency := make(map[string]int)
+	for _, word := range data {
+		// Remove unwanted characters like .,()- from the word
+		word = re.ReplaceAllString(word, "")
+		if len(word) == 0 {
+			continue
+		}
+		wordFrequency[strings.ToLower(word)]++
+	}
+
+	items := make([]sortItem, 0, len(wordFrequency))
+	for key, value := range wordFrequency {
+		items = append(items, sortItem{key, value})
+	}
+
+	sort.Slice(items, func(i, j int) bool {
+		if items[i].value == items[j].value {
+			return items[i].key < items[j].key
+		}
+		return items[i].value > items[j].value
+	})
+
+	var result []string
+
+	lenSize := 10
+	if len(items) < 10 {
+		lenSize = len(items)
+	}
+
+	for i := 0; i < lenSize; i++ {
+		result = append(result, items[i].key)
+	}
+
+	return result
 }
